0f5bb53e18f427fdd298366bdb6e746220c19d52
Merci4dev <merci4dev@gmail.com>
2022-12-27 04:35:02 -0600

    Delete post function added

--- main.py
+++ main.py
@@ -37,7 +37,12 @@ def find_post(id):
         if p["id"] == id:
             return p
 
-# function that finds the index of a post
+# Function which manage the logig to finds the index of an especific id into the dict 
+def find_index_post(id):
+    for i, p, in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+        print(i)
     
 
 # Path to the api home
@@ -93,3 +98,18 @@ def get_post(id: int, response: Response):
         # return {"message": f"Post with id [{id}] not found"}
 
     return {"post_details": post}
+
+
+
+# Handler Delete Function
+@app.delete("/posts/{id}", status_code = status.HTTP_404_NOT_FOUND)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    # Validation to avoid an error when we want delete a post which do not exists
+    if index == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Post with id [{id}] do not exists")
+
+    my_posts.pop(index)
+    # when we delete a post it will not send any data back
+    return Response( status_code = status.HTTP_404_NOT_FOUND)
