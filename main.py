fb17ce95fff392d5d4f63dc1313b4185cc8864f5
Merci4dev <merci4dev@gmail.com>
2022-12-27 04:25:53 -0600

    pydentic and Post model added at the main file

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # Modules import
 from fastapi import Body, FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 # Fastapi instantiation
 app = FastAPI()
 
+# Defining a basic model for the posts. throw this mode we make the validation for the data we want to recive
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    # Adding Optional values to the model
+    rating: Optional[int] = None
+    
 
 # Path to the api home
 @app.get("/")
@@ -12,6 +21,7 @@ async def root():
     return {"message": "Wellcome to my Ultimavive Api"}
 
 
+# Path to get all Post
 @app.get("/posts")
 def get_posts():
    return {"data": "This my List Post"}
@@ -20,8 +30,21 @@ def get_posts():
 # Post request to create the posts
 # The data willbe send throw the requeste boby
 @app.post("/posts")
-# Extrae la data del cuerpo de la request y lo convierte en un dicionario y lo guarda en la variable payload
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    print(type(payload))
-    return {"data": "Post successfully created"}
+def create_posts(post: Post):
+    # print(post.title)
+    # print(post.content)
+    # print(post.rating)
+    # print(type(post.rating))
+    # print(post)
+    ## Here the variable post is a pydentic model
+    # print(type(post))
+
+    print("***************************")
+    # With the dict method we convert the pydentic model into a dictionary
+    # print(post.dict())
+    print(post.dict())
+    print(type(post.dict()))
+
+    # return {"data","neww post"} ## rerturn an array
+    # return {"data": "neww post"}
+    return {"data": post}
